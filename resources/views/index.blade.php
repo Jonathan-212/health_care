0f0a60150e163bf7bec7c62e77fd0d472a1b2406
Jonathan-212 <frevia212@gmail.com>
2024-04-07 22:30:44 +0700

    added new layout and navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,23 @@
 @extends('layout')
 @section('content')
 
-    <div>
-        content
+    <div style="height:100%; width:calc(100% - 300px)">
+        <div style="display: flex; height:50%; justify-content:space-evenly; align-items:center">
+            <div class="shadowBox" style="width: 55%; height:90%">
+
+            </div>
+            <div class="shadowBox" style="width: 35%; height:90%">
+
+            </div>
+        </div>
+        <div style="display: flex; height:50%; justify-content:space-evenly; align-items:center">
+            <div class="shadowBox" style="width: 35%; height:90%">
+
+            </div>
+            <div class="shadowBox" style="width: 55%; height:90%">
+
+            </div>
+        </div>
     </div>
 
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <title>Navbar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
         .shadowBox {
@@ -15,16 +15,42 @@
             border-radius: 15px;
         }
         .btn-primary{
-            background-color:#E74646;
+            background-color:#142474;
         }
         .btn-primary:hover{
-            background-color:#FA9884;
+            background-color:#1e38bb;
+        }
+        html, body {
+            height: 100%;
+        }
+        .navbarr{
+            width: 300px;
+            background-color:#142474;
+            height:100%;
+            padding: 50px 40px;
+            color: white;
+            display: flex;
+            flex-direction: column;
+            position: relative;
+        }
+        .menu{
+            text-decoration: none; color:white; margin-bottom:15px
+        }
+        .menu:hover{
+            color:#FFF3E2;
         }
     </style>
 </head>
-<body style="background-color:#f0f0f0; display:flex">
-    <div class="navbar">
-        navbar
+<body style="background-color:#f0f0f0; display:flex;">
+    <div class="navbarr">
+        <a href="/" class="menu" style="margin-bottom:50px;"><div style="font-family: sans-serif; font-size:25px; font-weight:bold; ">HEALTHcare</div></a>
+
+        <a href="/" class="menu">Dashboard</a>
+        <a href="/consultation" class="menu">Doctor Consultation</a>
+        <a href="/medicine" class="menu">My Medicine</a>
+        <a href="/setting" class="menu">Setting</a>
+
+        <a href="/user/logout" class="menu" style="position: absolute; bottom:0px; margin-bottom: 50px">Logout</a>
     </div>
 
     @yield('content')
