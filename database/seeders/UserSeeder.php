63119966325f3e53d5d06d7cc8617aab31382ef6
Jonathan-212 <frevia212@gmail.com>
2024-04-14 16:32:09 +0700

    added user and doctor seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -23,9 +23,19 @@ class UserSeeder extends Seeder
             'weight' => 60,
             'phone' => '0812345678',
         ]);
-
         DB::table('users')->insert([
-            'name' => 'Nathanael',
+            'name' => 'Kenzo Gandha',
+            'email' => 'kenzo@gmail.com',
+            'password' => bcrypt('kenzo123'),
+            'date_of_birth' => '2004-05-12',
+            'height' => 170,
+            'weight' => 60,
+            'phone' => '0812345678',
+        ]);
+
+        // Bedah, THT, Gizi, Akupuntur, Jantung, Gigi
+        DB::table('users')->insert([
+            'name' => 'Dr. Natanael, Sp.B',
             'email' => 'nathanael@gmail.com',
             'password' => bcrypt('nathanael123'),
             'date_of_birth' => '2004-05-12',
@@ -33,6 +43,106 @@ class UserSeeder extends Seeder
             'weight' => 55,
             'phone' => '0812345678',
             'role' => 'doctor',
+            'specialist' => 'Bedah',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Dr. Mario, Sp.THT',
+            'email' => 'mario@gmail.com',
+            'password' => bcrypt('mariol123'),
+            'date_of_birth' => '2004-05-12',
+            'height' => 175,
+            'weight' => 55,
+            'phone' => '0812345678',
+            'role' => 'doctor',
+            'specialist' => 'THT',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Dr. Feronika, Sp.GK',
+            'email' => 'feronika@gmail.com',
+            'password' => bcrypt('feronikal123'),
+            'date_of_birth' => '2004-05-12',
+            'height' => 175,
+            'weight' => 55,
+            'phone' => '0812345678',
+            'role' => 'doctor',
+            'specialist' => 'Gizi',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Dr. Devon Wijaya, Sp.Ak',
+            'email' => 'devon@gmail.com',
+            'password' => bcrypt('devon123'),
+            'date_of_birth' => '2004-05-12',
+            'height' => 175,
+            'weight' => 55,
+            'phone' => '0812345678',
+            'role' => 'doctor',
+            'specialist' => 'Akupuntur',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Dr. Rizky Muhammad, Sp.JP',
+            'email' => 'rizky@gmail.com',
+            'password' => bcrypt('rizkyl123'),
+            'date_of_birth' => '2004-05-12',
+            'height' => 175,
+            'weight' => 55,
+            'phone' => '0812345678',
+            'role' => 'doctor',
+            'specialist' => 'Jantung',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Dr. Amelia, Sp.THT',
+            'email' => 'amelia@gmail.com',
+            'password' => bcrypt('amelia123'),
+            'date_of_birth' => '2004-05-12',
+            'height' => 175,
+            'weight' => 55,
+            'phone' => '0812345678',
+            'role' => 'doctor',
+            'specialist' => 'THT',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Dr. Robiyanto, Sp.KG',
+            'email' => 'robiyano@gmail.com',
+            'password' => bcrypt('robiyanto123'),
+            'date_of_birth' => '2004-05-12',
+            'height' => 175,
+            'weight' => 55,
+            'phone' => '0812345678',
+            'role' => 'doctor',
+            'specialist' => 'Gigi',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Dr. Richard, Sp.THT',
+            'email' => 'richard@gmail.com',
+            'password' => bcrypt('richard123'),
+            'date_of_birth' => '2004-05-12',
+            'height' => 175,
+            'weight' => 55,
+            'phone' => '0812345678',
+            'role' => 'doctor',
+            'specialist' => 'THT',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Dr. Sean Martin, Sp.KG',
+            'email' => 'sean@gmail.com',
+            'password' => bcrypt('sean123'),
+            'date_of_birth' => '2004-05-12',
+            'height' => 175,
+            'weight' => 55,
+            'phone' => '0812345678',
+            'role' => 'doctor',
+            'specialist' => 'Gigi',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Dr. Albert William, Sp.JP',
+            'email' => 'albert@gmail.com',
+            'password' => bcrypt('albert123'),
+            'date_of_birth' => '2004-05-12',
+            'height' => 175,
+            'weight' => 55,
+            'phone' => '0812345678',
+            'role' => 'doctor',
+            'specialist' => 'Jantung',
         ]);
     }
 }
