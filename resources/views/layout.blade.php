84e562c6e8bebd6186ee522f4ed43f4640b59f5c
Jonathan-212 <frevia212@gmail.com>
2024-04-16 23:02:01 +0700

    added jquery lib

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -48,6 +48,8 @@
             background-color: rgb(240, 240, 240);
         }
     </style>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body style="background-color:#f0f0f0; display:flex;">
     <div class="navbarr">
@@ -63,6 +65,5 @@
 
     @yield('content')
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
