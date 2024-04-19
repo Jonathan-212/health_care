e3d2c73e4eadf9fba94ac81f03617f304f9d1c7b
luckylaurens15 <luckylaurens1511@gmail.com>
2024-04-19 20:18:56 +0700

    added title

--- resources/views/healthyRecord.blade.php
+++ resources/views/healthyRecord.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 
-@section('title', 'Doctor List')
+@section('title', 'Healty Record')
 
 @section('content')
 
--- resources/views/videoCallMockUp.blade.php
+++ resources/views/videoCallMockUp.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 
-@section('title', 'Doctor List')
+@section('title', 'Video Call Consultation')
 
 @section('content')
 
