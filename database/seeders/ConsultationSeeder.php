36fb8ac52bf338f6e2139d793e52207bb5162174
luckylaurens15 <luckylaurens1511@gmail.com>
2024-04-19 23:54:47 +0700

    fix add lib

--- database/seeders/ConsultationSeeder.php
+++ database/seeders/ConsultationSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class ConsultationSeeder extends Seeder
